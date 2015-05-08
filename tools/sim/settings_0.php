0e5a351e238e64b9ed5f179d9983e3af5ca9b6c8
Edgar Sioson <esioson@uw.edu>
2015-05-07 22:32:14 -0700

    Move out sim directory from api/tools;

--- tools/sim/settings_0.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define("NUM_PERSONS", 20);
-define("CYCLE_MAX", 5);
-define("TICK_MAX", 10);
-
--- tools/sim/settings_1.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define("NUM_PERSONS", 40);
-define("CYCLE_MAX", 10);
-define("TICK_MAX", 15);
-
